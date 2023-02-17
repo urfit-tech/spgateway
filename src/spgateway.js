8a0123b8269e15f17f57603f4eee8146141760d0
Logan <clothe09986@gmail.com>
2023-02-17 18:43:28 +1100

    adjust spgateway new domain

--- src/spgateway.js
+++ src/spgateway.js
@@ -7,36 +7,35 @@ const CreditCardCloseService = require("./service/creditcard.close.service");
 const CreditCardCancelService = require("./service/creditcard.cancel.service");
 
 module.exports = class SpGateway {
+  constructor(dryRun, merchantID, hashKey, hashIV, debug) {
+    this.configuration = {
+      host: (dryRun ? "c" : "") + "core.newebpay.com",
+      MerchantID: merchantID,
+      HashKey: hashKey,
+      HashIV: hashIV,
+    };
+    logger.switchLog(debug);
+    log.debug("configuration applied", this.configuration);
+  }
 
-    constructor(dryRun, merchantID, hashKey, hashIV, debug) {
-        this.configuration = {
-            host: (dryRun ? "c" : "") + "core.spgateway.com",
-            MerchantID: merchantID,
-            HashKey: hashKey,
-            HashIV: hashIV
-        };
-        logger.switchLog(debug);
-        log.debug("configuration applied", this.configuration);
-    }
+  createMpgService() {
+    return new MpgService(this.configuration);
+  }
 
-    createMpgService() {
-        return new MpgService(this.configuration);
-    }
+  createPeriodicalService() {
+    return new PeriodicalService(this.configuration);
+  }
 
-    createPeriodicalService() {
-        return new PeriodicalService(this.configuration);
-    }
+  createTradeInfoService() {
+    return new TradeInfoService(this.configuration);
+  }
 
-    createTradeInfoService() {
-        return new TradeInfoService(this.configuration);
-    }
+  createCreditCardCloseService() {
+    return new CreditCardCloseService(this.configuration);
+  }
 
-    createCreditCardCloseService() {
-        return new CreditCardCloseService(this.configuration);
-    }
-
-    createCreditCardCancelService() {
-        return new CreditCardCancelService(this.configuration);
-    }
-
+  createCreditCardCancelService() {
+    return new CreditCardCancelService(this.configuration);
+  }
 };
+
