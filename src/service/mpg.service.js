a9646459178629542c998e0206ad1951fce511bd
Logan <clothe09986@gmail.com>
2025-02-17 13:04:30 +0800

    change version to 2.2

--- src/service/mpg.service.js
+++ src/service/mpg.service.js
@@ -11,93 +11,103 @@ const modelPivot = require("../model/model.pivot");
 const MpgPayModel = modelPivot.MPG.MpgPayModel;
 const MpgNotifyModel = modelPivot.MPG.MpgNotifyModel;
 
-
-const spApiVersion = "1.1";
+const spApiVersion = "2.2";
 
 class MpgService {
-    /**
-     *
-     * @param {Configuration} config
-     */
-    constructor(config) {
-        this.config = config;
-        this.apiUrl = `https://${config.host}/MPG/mpg_gateway`;
-        this.validationHelper = new ValidationHelper(config);
-    }
-    
-    
-    /**
-     * 
-     * @returns {MpgPayModel}
-     */
-    createMpgPayModel() {
-        return new MpgPayModel();
-    }
+  /**
+   *
+   * @param {Configuration} config
+   */
+  constructor(config) {
+    this.config = config;
+    this.apiUrl = `https://${config.host}/MPG/mpg_gateway`;
+    this.validationHelper = new ValidationHelper(config);
+  }
 
-    /**
-     * 付款 html form
-     * @returns {string}
-     */
-    getAutoPayForm(payModel) {
-        let model = new MpgPayModel();
-        model = Object.assign(model, payModel);
-        model.Version = model.Version || spApiVersion;
-        model.MerchantID = this.config.MerchantID;
+  /**
+   *
+   * @returns {MpgPayModel}
+   */
+  createMpgPayModel() {
+    return new MpgPayModel();
+  }
 
-        model.TokenTerm = shaEncrypt.encrypt(model.Email).toUpperCase();
-        model.CheckValue = this.validationHelper.genMpgCheckValue(model.Amt, model.MerchantOrderNo, model.TimeStamp, model.Version);
-        log.debug("payModel", payModel);
-        let html = payFormGenerator(model, this.apiUrl);
-        log.debug("payFormHtml", html);
-        return html;
-    }
-    
-    /**
-     * 付款 form data
-     * @returns {string}
-     */
-    getAutoPayData(payModel) {
-        let model = new MpgPayModel();
-        model = Object.assign(model, payModel);
-        model.Version = model.Version || spApiVersion;
-        model.MerchantID = this.config.MerchantID;
+  /**
+   * 付款 html form
+   * @returns {string}
+   */
+  getAutoPayForm(payModel) {
+    let model = new MpgPayModel();
+    model = Object.assign(model, payModel);
+    model.Version = model.Version || spApiVersion;
+    model.MerchantID = this.config.MerchantID;
 
-        model.TokenTerm = shaEncrypt.encrypt(model.Email).toUpperCase();
-        model.CheckValue = this.validationHelper.genMpgCheckValue(model.Amt, model.MerchantOrderNo, model.TimeStamp, model.Version);
-        log.debug("payModel", payModel);
+    model.TokenTerm = shaEncrypt.encrypt(model.Email).toUpperCase();
+    model.CheckValue = this.validationHelper.genMpgCheckValue(
+      model.Amt,
+      model.MerchantOrderNo,
+      model.TimeStamp,
+      model.Version
+    );
+    log.debug("payModel", payModel);
+    let html = payFormGenerator(model, this.apiUrl);
+    log.debug("payFormHtml", html);
+    return html;
+  }
+
+  /**
+   * 付款 form data
+   * @returns {string}
+   */
+  getAutoPayData(payModel) {
+    let model = new MpgPayModel();
+    model = Object.assign(model, payModel);
+    model.Version = model.Version || spApiVersion;
+    model.MerchantID = this.config.MerchantID;
+
+    model.TokenTerm = shaEncrypt.encrypt(model.Email).toUpperCase();
+    model.CheckValue = this.validationHelper.genMpgCheckValue(
+      model.Amt,
+      model.MerchantOrderNo,
+      model.TimeStamp,
+      model.Version
+    );
+    log.debug("payModel", payModel);
+    return model;
+  }
+
+  /**
+   *
+   * @param {string} jsonString
+   * @returns {Promise<MpgNotifyModel>}
+   * VACC: {"Status":"SUCCESS","Message":"取號成功","Result":{"MerchantID":"MS11237061","Amt":101,"TradeNo":"17040700413810767","MerchantOrderNo":"1491496889809","CheckCode":"91EAD5C8290BABAD4FB99024DBDB56C99336665BCBC117B2B04B9536EA528A6E","PaymentType":"VACC","ExpireDate":"2017-04-14","ExpireTime":"23:59:59","BankCode":"808","CodeNo":"TestAccount12345"}}
+   * CREDIT :{ Status: 'SUCCESS', Message: '授權成功', Result: { MerchantID: 'MS11237061', Amt: 101, TradeNo: '17040612171797240', MerchantOrderNo: '1491452222783', RespondType: 'JSON', CheckCode: '626B373EEBE8AA4541D91B738477115B564361C4DC7C0E97516B8B4902C9B3CC', IP: '1.173.74.76', EscrowBank: 'KGI', PaymentType: 'CREDIT', RespondCode: '00', Auth: '930637', Card6No: '400022', Card4No: '1111', Exp: '2303', TokenUseStatus: '2', InstFirst: 101, InstEach: 0, Inst: 0, ECI: '', PayTime: '2017-04-06 12:17:17' } }
+   */
+  parseNotification(jsonString) {
+    let model = JSON.parse(jsonString || "{}");
+    model.Result = JSON.parse(model.Result || "{}");
+    let resultModel = model.Result;
+    let checkCode = this.validationHelper.genMpgCheckCode(
+      resultModel.Amt,
+      resultModel.MerchantOrderNo,
+      resultModel.TradeNo
+    );
+    return Promise.resolve().then(() => {
+      if (checkCode === resultModel.CheckCode) {
         return model;
-    }
+      } else {
+        return Promise.reject(new Error("CheckCode Validation Failed"));
+      }
+    });
+  }
 
-
-    /**
-     *
-     * @param {string} jsonString
-     * @returns {Promise<MpgNotifyModel>}
-     * VACC: {"Status":"SUCCESS","Message":"取號成功","Result":{"MerchantID":"MS11237061","Amt":101,"TradeNo":"17040700413810767","MerchantOrderNo":"1491496889809","CheckCode":"91EAD5C8290BABAD4FB99024DBDB56C99336665BCBC117B2B04B9536EA528A6E","PaymentType":"VACC","ExpireDate":"2017-04-14","ExpireTime":"23:59:59","BankCode":"808","CodeNo":"TestAccount12345"}}
-     * CREDIT :{ Status: 'SUCCESS', Message: '授權成功', Result: { MerchantID: 'MS11237061', Amt: 101, TradeNo: '17040612171797240', MerchantOrderNo: '1491452222783', RespondType: 'JSON', CheckCode: '626B373EEBE8AA4541D91B738477115B564361C4DC7C0E97516B8B4902C9B3CC', IP: '1.173.74.76', EscrowBank: 'KGI', PaymentType: 'CREDIT', RespondCode: '00', Auth: '930637', Card6No: '400022', Card4No: '1111', Exp: '2303', TokenUseStatus: '2', InstFirst: 101, InstEach: 0, Inst: 0, ECI: '', PayTime: '2017-04-06 12:17:17' } }
-     */
-    parseNotification(jsonString) {
-        let model = JSON.parse(jsonString || "{}");
-        model.Result = JSON.parse(model.Result || "{}");
-        let resultModel = model.Result;
-        let checkCode = this.validationHelper.genMpgCheckCode(resultModel.Amt, resultModel.MerchantOrderNo, resultModel.TradeNo);
-        return Promise
-            .resolve()
-            .then(() => {
-                if (checkCode === resultModel.CheckCode) {
-                    return model;
-                } else {
-                    return Promise.reject(new Error("CheckCode Validation Failed"));
-                }
-            });
-    }
-    
-    /**
-     * @returns {modelPivot.MPG}
-     */
-    get Models() {
-        return modelPivot.MPG;
-    }
+  /**
+   * @returns {modelPivot.MPG}
+   */
+  get Models() {
+    return modelPivot.MPG;
+  }
 }
 
 module.exports = MpgService;
+
